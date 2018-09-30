fb756ede29d14b31dc225367b0b614dba769e612
Kirill Trofimov <kirilltrofimov@inbox.lv>
2018-09-30 23:27:41 +0300

    Added currency parser. Gets all data about seller and price of item

--- src/instance.cpp
+++ src/instance.cpp
@@ -12,13 +12,13 @@ bool Instance::Initialize(char* path_to_exefile_)
     m_path_to_exefile = path_to_exefile_;
     m_path_to_dir = m_path_to_exefile.substr(0, m_path_to_exefile.find_last_of("/\\"));
 
-    m_base_url = "http://currency.poe.trade";
+    m_base_url = "http://currency.poe.trade/";
     m_league_name = "Delve";
 
     curl_version_info_data* c_inf = curl_version_info(CURLVERSION_NOW);
 
     /// Because i need to inflate gzipped html page
-    if(strcmp(c_inf->libz_version, "1.2.8") != 0 || strcmp(c_inf->version, "7.62.0-DEV") != 0)
+    if(std::string(c_inf->libz_version) != "1.2.8" || std::string(c_inf->version) != "7.62.0-DEV")
     {
         std::cout << "Bad curl.dll version." << std::endl;
         return false;
@@ -31,8 +31,19 @@ bool Instance::Initialize(char* path_to_exefile_)
 
 bool Instance::Run()
 {
-    std::string PageCode = GetPage(m_base_url);
-    //std::cout << PageCode << std::endl;
+    auto get_itemprice_responce = GetItemPrice(true, EXALTED_ORB, CHAOS_ORB, 0);
+    if(!get_itemprice_responce.second)
+    {
+        std::cerr << "Failed to retrieve item price." << std::endl;
+        return false;
+    }
+
+    std::vector<Item> ex_chaos_prices = get_itemprice_responce.first;
+
+    for(auto it = ex_chaos_prices.begin(); it != ex_chaos_prices.end(); it++)
+    {
+        std::cout << it->s_buy_price << std::endl;
+    }
 
     return true;
 }
@@ -43,7 +54,7 @@ void Instance::Shutdown()
     curl_easy_cleanup(m_curl_ptr);
 }
 
-std::string Instance::GetPage(const std::string& page_address_)
+std::pair<std::string, bool> Instance::GetPage(const std::string& page_address_)
 {
     if(m_curl_ptr)
     {
@@ -59,15 +70,91 @@ std::string Instance::GetPage(const std::string& page_address_)
         result = curl_easy_perform(m_curl_ptr);
         if(result != CURLE_OK)
         {
-            std::cerr << "curl_easy_perform error: " << curl_easy_strerror(result) << std::endl;
-            return std::string("");
+            std::cerr << curl_easy_strerror(result) << std::endl;
+            return std::pair<std::string, bool>(std::string(""), false);
         }
 
-        return buffer;
+        return std::pair<std::string, bool>(buffer, true);
     }
     else
     {
-        std::cout << "Failed to get page. Curl is not initalized" << std::endl;
-        return std::string("");
+        std::cerr << "Failed to get page. Curl is not initalized" << std::endl;
+        return std::pair<std::string, bool>(std::string(""), false);
     }
+}
+
+std::pair<std::vector<Item>, bool> Instance::GetItemPrice(bool online_only_, int item_type_, int price_item_type_, int min_stock_)
+{
+    TradeURL trade_url(m_base_url, m_league_name, online_only_, item_type_, price_item_type_, min_stock_);
+    trade_url.Compose();
+    auto page_url = trade_url.Get();
+    auto get_page_response = GetPage(page_url);
+    std::vector<Item> temp_item_vec;
+
+    if(!get_page_response.second)
+    {
+        std::cerr << "Failed to get item price." << std::endl;
+        return std::pair<std::vector<Item>, bool>(std::vector<Item>(), false);
+    }
+
+    std::string page_code = get_page_response.first;
+    std::string block_start_code = "<div class=\"displayoffer \"";
+    std::string block_end_code = ">";
+
+    std::string data_username_code = "data-username";
+    std::string data_ign_code = "data-ign";
+    std::string data_sellvalue_code = "data-sellvalue";
+    std::string data_buyvalue_code = "data-buyvalue";
+    std::string data_stock_code = "data-stock";
+
+    auto occurrence_it = std::search(page_code.begin(), page_code.end(), block_start_code.begin(), block_start_code.end());
+    if(occurrence_it == page_code.end())
+    {
+        std::cerr << "Failed to find first div occurrence." << std::endl;
+        return std::pair<std::vector<Item>, bool>(std::vector<Item>(), false);
+    }
+    page_code.erase(page_code.begin(), occurrence_it);
+
+    while(true)
+    {
+        auto block_start_occurrence_it = std::search(page_code.begin(), page_code.end(), block_start_code.begin(), block_start_code.end());
+        auto block_end_occurrence_it = std::search(block_start_occurrence_it, page_code.end(), block_end_code.begin(), block_end_code.end());
+
+        if(block_end_occurrence_it == page_code.end() || block_start_occurrence_it == page_code.end())
+            break;
+        else
+        {
+            bool is_stock_present = true;
+
+            std::string data_block(block_start_occurrence_it, block_end_occurrence_it + 2);
+            page_code.erase(page_code.begin(), block_end_occurrence_it);
+
+            auto acc_name_it = std::search(data_block.begin(), data_block.end(), data_username_code.begin(), data_username_code.end()) + data_username_code.size() + 1;
+            auto ign_it = std::search(data_block.begin(), data_block.end(), data_ign_code.begin(), data_ign_code.end()) + data_ign_code.size() + 1;
+            auto sellvalue_it = std::search(data_block.begin(), data_block.end(), data_sellvalue_code.begin(), data_sellvalue_code.end()) + data_sellvalue_code.size() + 1;
+            auto buyvalue_it = std::search(data_block.begin(), data_block.end(), data_buyvalue_code.begin(), data_buyvalue_code.end()) + data_buyvalue_code.size() + 1;
+            auto stock_it = std::search(data_block.begin(), data_block.end(), data_stock_code.begin(), data_stock_code.end()) + data_stock_code.size() + 1;
+            if(stock_it - data_stock_code.size() - 1 == data_block.end())
+                is_stock_present = false;
+
+            int acc_name_dist = std::distance(data_block.begin(), acc_name_it);
+            int ign_dist = std::distance(data_block.begin(), ign_it);
+            int sellvalue_dist = std::distance(data_block.begin(), sellvalue_it);
+            int buyvalue_dist = std::distance(data_block.begin(), buyvalue_it);
+            int stock_dist = std::distance(data_block.begin(), stock_it);
+
+            std::string acc_name = data_block.substr(acc_name_dist + 1, data_block.find("\"", acc_name_dist + 1) - acc_name_dist - 1);
+            std::string char_name = data_block.substr(ign_dist + 1, data_block.find("\"", ign_dist + 1) - ign_dist - 1);
+            std::string sellvalue = data_block.substr(sellvalue_dist + 1, data_block.find("\"", sellvalue_dist + 1) - sellvalue_dist - 1);
+            std::string buyvalue = data_block.substr(buyvalue_dist + 1, data_block.find("\"", buyvalue_dist + 1) - buyvalue_dist - 1);
+            std::string stock = "0";
+            if(is_stock_present)
+                stock = data_block.substr(stock_dist + 1, data_block.find("\"", stock_dist + 1) - stock_dist - 1);
+
+            Item temp_item {item_type_, price_item_type_, std::stof(sellvalue), std::stof(buyvalue), std::stoi(stock), acc_name, char_name};
+            temp_item_vec.push_back(temp_item);
+        }
+    }
+
+    return std::pair<std::vector<Item>, bool>(temp_item_vec, true);
 }
--- src/instance.h
+++ src/instance.h
@@ -3,12 +3,23 @@
 #include <string>
 #include <iostream>
 #include <vector>
+#include <utility>
+#include <algorithm>
 
 #include "include/curl/curl.h"
 
-struct MemoryStruct {
-    char* memory;
-    size_t size;
+struct Item
+{
+    int s_item_type;
+    int s_price_item_type;
+
+    float s_sell_price;
+    float s_buy_price;
+
+    int s_stock;
+
+    std::string s_seller_acc_name;
+    std::string s_seller_char_name;
 };
 
 class Instance
@@ -21,7 +32,8 @@ public:
     bool Run();
     void Shutdown();
 
-    std::string GetPage(const std::string& page_address_);
+    std::pair<std::string, bool>       GetPage(const std::string& page_address_);
+    std::pair<std::vector<Item>, bool> GetItemPrice(bool online_only_, int item_type_, int price_item_type_, int min_stock_);
 
 private:
     std::string m_path_to_exefile;
@@ -31,4 +43,43 @@ private:
     std::string m_league_name;
 
     CURL* m_curl_ptr = nullptr;
+};
+
+class TradeURL
+{
+public:
+    TradeURL(std::string base_url_, std::string league_name_, bool online_, int want_, int have_, int stock_) :
+        m_base_url(base_url_), m_league_name(league_name_), m_online(online_), m_want(want_), m_have(have_), m_stock(stock_) {}
+    ~TradeURL() {}
+
+    void SetBaseURL(const std::string& base_url_) { m_base_url = base_url_; }
+    void SetLeagueName(const std::string& league_name_) { m_league_name = league_name_; }
+    void SetOnlineOnly(bool online_) { m_online = online_; }
+    void SetWantedItem(int want_) { m_want = want_; }
+    void SetPresentItem(int have_) { m_have = have_; }
+    void SetMinStock(int stock_) { m_stock = stock_; }
+
+    void Compose()
+    {
+        m_composed_url.append(m_base_url);
+        m_composed_url.append("search?");
+        m_composed_url.append("league=");  m_composed_url.append(m_league_name);
+        m_composed_url.append("&online="); m_composed_url.append(m_online?"x":"");
+        m_composed_url.append("&stock=");  m_composed_url.append(std::to_string(m_stock));
+        m_composed_url.append("&want=");   m_composed_url.append(std::to_string(m_want));
+        m_composed_url.append("&have=");   m_composed_url.append(std::to_string(m_have));
+    }
+
+    std::string Get() { return m_composed_url; }
+
+
+private:
+    std::string m_league_name;
+    bool m_online;
+    int m_want;
+    int m_have;
+    int m_stock;
+
+    std::string m_base_url;
+    std::string m_composed_url;
 };
